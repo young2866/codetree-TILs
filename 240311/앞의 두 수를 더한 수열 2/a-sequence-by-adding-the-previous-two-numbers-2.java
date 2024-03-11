f5eea46c13fb5d48007b9596b860d119d5fa9b44
young2866 <62212610+young2866@users.noreply.github.com>
2024-03-11 11:05:29 +0900

    Title: 앞의 두 수를 더한 수열 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240311/앞의 두 수를 더한 수열 2/a-sequence-by-adding-the-previous-two-numbers-2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n + 1];
+
+        arr[1] = 1;
+        arr[2] = 1;
+
+        for(int i = 3; i <= n; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        System.out.println(arr[n]);
+        // 여기에 코드를 작성해주세요.
+    }
+}
