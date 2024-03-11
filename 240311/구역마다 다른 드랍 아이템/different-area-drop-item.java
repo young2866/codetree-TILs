311a3be1ef6817eb514b8e2c269c8c1ef42fbb71
young2866 <62212610+young2866@users.noreply.github.com>
2024-03-11 17:27:24 +0900

    Title: 구역마다 다른 드랍 아이템, Time: 107ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240311/구역마다 다른 드랍 아이템/different-area-drop-item.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        int[] map = new int[N + 1];
+        Arrays.fill(map, 1);
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            pq.add(new Pair(Math.min(a, b), Math.max(a, b)));
+        }
+
+        while(!pq.isEmpty()) {
+            Pair temp = pq.poll();
+            int first = temp.first;
+            int second = temp.second;
+
+            if(map[first] == map[second])
+                map[second]++;
+        }
+
+        for(int i = 1; i <= N; i++) {
+            System.out.print(map[i]);
+        }
+
+        // 여기에 코드를 작성해주세요.
+    }
+    static class Pair implements Comparable<Pair>{
+        int first;
+        int second;
+
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        @Override
+        public int compareTo(Pair o1) {
+            if(o1.first == this.first) {
+                return this.second - o1.second;
+            }
+            return this.first - o1.first;
+        }
+    }
+}
