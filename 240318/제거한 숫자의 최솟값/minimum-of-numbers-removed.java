cf6934434d4470012df0d9aa4f0527f3002df53a
young2866 <62212610+young2866@users.noreply.github.com>
2024-03-18 22:25:01 +0900

    Title: 제거한 숫자의 최솟값, Time: 241ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240318/제거한 숫자의 최솟값/minimum-of-numbers-removed.java
+++ 240318/제거한 숫자의 최솟값/minimum-of-numbers-removed.java
@@ -8,19 +8,22 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         String[] split = br.readLine().split(" ");
-        Arrays.sort(split);
+        Long[] longArray = Arrays.stream(split)
+                                  .mapToLong(Long::parseLong)
+                                  .boxed()
+                                  .toArray(Long[]::new);
+        Arrays.sort(longArray);
         if(split.length % 2 == 0) {
             Long max = 0L;
             for(int i = 0; i <= split.length / 2; i++) {
-                max = Math.max(Long.parseLong(split[i]) + Long.parseLong(split[split.length - i - 1]), max);
+                max = Math.max(longArray[i] + longArray[split.length - i - 1], max);
             }
-            System.out.println(max);
         }else {
             Long max = 0L;
             for(int i = 0; i <= split.length / 2 - 1; i++) {
-                max = Math.max(Long.parseLong(split[i]) + Long.parseLong(split[split.length - i - 2]), max);
+                max = Math.max(longArray[i] + longArray[split.length - i - 2], max);
             }
-            max = Math.max(max, Long.parseLong(split[split.length - 1]));
+            max = Math.max(max, longArray[split.length - 1]);
             System.out.println(max);
         }
 
