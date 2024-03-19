087f9ad8a89d854d424b96465bbc33d7a6f1e2d8
young2866 <62212610+young2866@users.noreply.github.com>
2024-03-19 20:42:28 +0900

    Title: 최대 이익 구하기 2, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240319/최대 이익 구하기 2/find-the-maximum-profit-2.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static List<int[]> ll;
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        ll = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            ll.add(new int[]{a, b});
+        }
+        func(0, 0);
+        
+        System.out.println(count);
+        // 여기에 코드를 작성해주세요.
+    }
+    static void func(int now, int sum) {
+        if(now == ll.size()) {
+            count = Math.max(sum, count);
+            return;
+        }
+        int[] a = ll.get(now);
+        if(a[0] == 1) {
+            func(now + 1, sum + a[1]);
+        }
+        if(a[0] != 1 && now + a[0] < ll.size()) {
+            func(now + a[0], sum + a[1]);
+        }
+        else {
+            func(now + 1, sum);
+        }
+    }
+}
