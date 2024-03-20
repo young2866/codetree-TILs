25c183a701de5fa3624475cef01aecbe0d9e2965
young2866 <62212610+young2866@users.noreply.github.com>
2024-03-20 19:26:56 +0900

    Title: 조건에 따른 이진수, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

--- 240320/조건에 따른 이진수/binary-numbers-based-on-conditions.java
+++ 240320/조건에 따른 이진수/binary-numbers-based-on-conditions.java
@@ -10,17 +10,15 @@ public class Main {
 
         n = Integer.parseInt(br.readLine());
 
-        int[] arr= new int[n+1];
-        arr[1] = 1;
-        arr[2] = 1;
+        Long[] arr= new Long[n+1];
+        arr[1] = 1L;
+        arr[2] = 1L;
         for(int i = 3; i<=n; i++) {
             arr[i] = arr[i-1] + arr[i-2];
         }
 
         System.out.println(arr[n]);
         // 여기에 코드를 작성해주세요.
-    }
-    static void func(String target) {
     }
 
 }
