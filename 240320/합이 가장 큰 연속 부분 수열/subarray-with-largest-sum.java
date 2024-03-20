0f2bd4e2e8b782e2a917eb86ca42be35b0c1f12e
young2866 <62212610+young2866@users.noreply.github.com>
2024-03-20 23:03:54 +0900

    Title: 합이 가장 큰 연속 부분 수열, Time: 255ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 240320/합이 가장 큰 연속 부분 수열/subarray-with-largest-sum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dp[0] = arr[0];
+        for(int i = 1; i < n; i++) {
+            dp[i] = (dp[i] > dp[i-1] + arr[i]) ? dp[i] : dp[i-1] + arr[i];
+        }
+        int max = 0;
+        for(int i = 0; i < n; i++) {
+            max = Math.max(dp[i], max);
+        }
+        System.out.println(max);
+
+        // 여기에 코드를 작성해주세요.
+    }
+}
