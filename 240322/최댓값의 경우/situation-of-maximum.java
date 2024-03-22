ac4b2cb8ee3ebd617d97e071301555347720dff9
young2866 <62212610+young2866@users.noreply.github.com>
2024-03-22 21:29:13 +0900

    Title: 최댓값의 경우, Time: 3016ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

--- 240322/최댓값의 경우/situation-of-maximum.java
+++ 240322/최댓값의 경우/situation-of-maximum.java
@@ -21,23 +21,23 @@ public class Main {
             int b = Integer.parseInt(st.nextToken());
             ll.add(new int[]{a,b});
         }
-        func(chk, d, 100001);
+        func(chk, d, 100001,0);
         System.out.println(min);
         // 여기에 코드를 작성해주세요.
     }
 
-    static void func(boolean[] chk, int target, int now) {
+    static void func(boolean[] chk, int target, int now, int index) {
         if(target == 0) {
             min = Math.max(now, min);
         }
 
-        for(int i = 0; i < chk.length; i++) {
+        for(int i = index; i < chk.length; i++) {
             if(chk[i] == false) {
                 int[] temp = ll.get(i);
                 if(target - temp[0] >= 0) {
                     chk[i] = true;
                     int min_temp = Math.min(now, temp[1]);
-                    func(chk, target-temp[0], min_temp);
+                    func(chk, target-temp[0], min_temp, i+1);
                     chk[i] = false;
                 }
             }
