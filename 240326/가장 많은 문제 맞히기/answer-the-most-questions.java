b21f08cc646c4459e9b5152db6bf3389c1e75f0d
young2866 <62212610+young2866@users.noreply.github.com>
2024-03-26 22:53:35 +0900

    Title: 가장 많은 문제 맞히기, Time: 3038ms, Memory: 13MB, Status: Time Limit Exceed - CodeTree

---
+++ 240326/가장 많은 문제 맞히기/answer-the-most-questions.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static int max = 0;
+    static List<int[]> ll;
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        ll = new ArrayList<>();
+        
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            ll.add(new int[]{a,b});
+        }
+
+        Collections.sort(ll, (int[] a, int[] b) -> {return a[0] - b[0];});
+
+        func(0, 0, t);
+
+        System.out.println(max);
+
+        // 여기에 코드를 작성해주세요.
+    }
+    static void func(int start, int sum, int now) {
+        if(start == n) {
+            max = Math.max(sum, max);
+            return;
+        }
+
+        int[] temp = ll.get(start);
+        if(now - temp[0] >= 0) {
+            func(start + 1, sum + temp[1], now-temp[0]);
+        }
+        func(start + 1, sum, now);
+    }
+}
